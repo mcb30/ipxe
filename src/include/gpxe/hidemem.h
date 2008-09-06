218ba3f7c38c173e07874be118be1fa0ae6e6890
Michael Brown <mcb30@etherboot.org>
2008-09-07 00:12:29 +0100

    Revert "[pcbios] Support arbitrary splits of the e820 memory map"
    This reverts commit 320b072c7a599fe1dab2774f030dd0bedef6f047.

--- src/include/gpxe/hidemem.h
+++ src/include/gpxe/hidemem.h
@@ -8,8 +8,16 @@
  *
  */
 
-#include <stdint.h>
+/**
+ * Unique IDs for hidden regions
+ */
+enum hidemem_region_id {
+	TEXT = 0,
+	BASEMEM,
+	EXTMEM,
+};
 
-extern void hide_umalloc ( physaddr_t start, physaddr_t end );
+extern void hide_region ( unsigned int region_id, physaddr_t start,
+			  physaddr_t end );
 
 #endif /* _GPXE_HIDEMEM_H */
