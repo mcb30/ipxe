02fc3a9b96f56a0606d506ff90d2e3d4aedda4ee
Michael Brown <mcb30@ipxe.org>
2014-03-21 11:19:49 +0000

    Try clearing only a 15x15 character block

--- src/arch/i386/firmware/pcbios/bios_console.c
+++ src/arch/i386/firmware/pcbios/bios_console.c
@@ -99,11 +99,11 @@ static void bios_handle_ed ( struct ansiesc_context *ctx __unused,
 
 	__asm__ __volatile__ ( REAL_CODE ( "pushal\n\t"
 					   "sti\n\t"
-					   //   "int $0x10\n\t"
+					   "int $0x10\n\t"
 					   "cli\n\t"
 					   "popal\n\t" )
 			       : : "a" ( 0x0600 ), "b" ( bios_attr << 8 ),
-			           "c" ( 0 ), "d" ( 0xfefe ) );
+			           "c" ( 0 ), "d" ( 0x0f0f ) );
 }
 
 /**
