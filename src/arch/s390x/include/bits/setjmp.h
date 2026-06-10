482823d099621a6c09013e1f4cfef25976c01f45
Michael Brown <mcb30@ipxe.org>
2026-06-10 18:24:24 +0100

    WIP

--- src/arch/s390x/include/bits/setjmp.h
+++ src/arch/s390x/include/bits/setjmp.h
@@ -5,12 +5,8 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 
 /** A jump buffer */
 typedef struct {
-	/** Return address (ra) */
-	unsigned long ra;
-	/** Stack pointer (sp) */
-	unsigned long sp;
-	/** Callee-saved registers (s0-s11) */
-	unsigned long s[12];
+	/** Callee-saved registers (r6-r15) */
+	unsigned long regs[12];
 } jmp_buf[1];
 
 #endif /* _BITS_SETJMP_H */
